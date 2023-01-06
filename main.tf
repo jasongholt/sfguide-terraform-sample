73f8ddc4cff5a7a57e3035c83dcb2caa11a2d7de
jasongholt <jason.grant.holt@gmail.com>
2023-01-06 00:50:19 -0500

    new

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "wh_setup" {
   user_role_id = "SECURITYADMIN"
   db_name = "TF_DEMO_DB"
   wh_name = "TF_DEMO_WH"
-  wh_size = "small"
+  wh_size = "large"
   svc_role_name = "TF_DEMO_SVC_ROLE"
   schema_name = "TF_DEMO_SCHEMA"
   user_id = "tf_demo_user"
