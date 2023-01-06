79ef43ca4b1a406f13a36ca0497be647b5d80cd2
jasongholt <jason.grant.holt@gmail.com>
2023-01-05 22:36:21 -0500

    change to large

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-module "wh_setup" {
+-module "wh_setup" {
 
   source = "github.com/jasongholt/sfguide-terraform-sample/modules"
   admin_role_id = "SYSADMIN"
   user_role_id = "SECURITYADMIN"
   db_name = "TF_DEMO_DB"
   wh_name = "TF_DEMO_WH"
-  wh_size = "small"
+  wh_size = "large"
   svc_role_name = "TF_DEMO_SVC_ROLE"
   schema_name = "TF_DEMO_SECHEMA"
   user_id = "tf_demo_user"
